3bd441d58d21131b855f5a40c2ac6ffb0ad49b9b
yuuv12 <112937019+yuuv12@users.noreply.github.com>
2023-12-28 15:45:14 +0800

    Delete frontend directory
    rebuild project

--- frontend/gtag.js
+++
@@ -1,9 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-
-gtag('config', 'G-D1XNH66KRX');
-gtag('config', 'AW-11300378071')
