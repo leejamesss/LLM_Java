898eba9254073f84577f85a9974f6d73bcd6d61a
yuuv12 <112937019+yuuv12@users.noreply.github.com>
2023-12-28 15:45:41 +0800

    Delete langchain directory
    rebuild project

--- langchain/client.py
+++
@@ -1,7 +0,0 @@
-from langserve import RemoteRunnable
-
-remote_chain = RemoteRunnable("http://localhost:8000/category_chain/")
-
-test = remote_chain.invoke({"text": "colors"})
-print(test)
-# >> ['red', 'blue', 'green', 'yellow', 'orange']
