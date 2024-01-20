4d2eb48c3208b1e891f49f3a5ce379b24830d04c
yuuv12 <112937019+yuuv12@users.noreply.github.com>
2024-01-20 23:51:08 +0800

    Update mysqlforllm-5.X.sql

--- GptDemo/mysqlforllm-5.X.sql
+++ GptDemo/mysqlforllm-5.X.sql
@@ -33,4 +33,18 @@ CREATE TABLE `dblogforllm`  (
                                 `Hrcv` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of dblogforllm
+-- ----------------------------
+INSERT INTO `dblogforllm` VALUES (1, 'Succeeded', '我和小明去了北京大学', '北京大学-中性-去了');
+INSERT INTO `dblogforllm` VALUES (2, 'Chat', '你好', '你好! 有什么我能帮助您的吗？');
+INSERT INTO `dblogforllm` VALUES (2, 'Chat', '我想睡觉', '那你可以去休息一下，好好放松一下。晚安！');
+INSERT INTO `dblogforllm` VALUES (3, 'Chat', '你好\n', '你好！有什么可以帮助你的吗？');
+INSERT INTO `dblogforllm` VALUES (3, 'Chat', '我想睡觉了今天\n', '好的，希望你有个美好的睡眠！晚安！');
+INSERT INTO `dblogforllm` VALUES (4, 'ASTE', '我现在想去睡不着\n', '');
+INSERT INTO `dblogforllm` VALUES (5, 'ASTE', '我想区睡觉\n', '睡觉-中性-想');
+INSERT INTO `dblogforllm` VALUES (6, 'Translated', 'good night\n', '晚安');
+INSERT INTO `dblogforllm` VALUES (7, 'Chat', 'goood\n', 'I\'m glad to hear that! Is there anything specific you would like to talk about or do?');
+INSERT INTO `dblogforllm` VALUES (7, 'Chat', 'helloc\n', 'Hello! How can I assist you today?');
+
 SET FOREIGN_KEY_CHECKS = 1;
