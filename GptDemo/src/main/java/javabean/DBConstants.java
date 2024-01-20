5fa5ebbee57862431fc7e07a2da15c0f88456b0d
jiatao <2200016651@stu.pku.edu.cn>
2024-01-20 14:48:21 +0800

    fix stream style

--- GptDemo/src/main/java/javabean/DBConstants.java
+++ GptDemo/src/main/java/javabean/DBConstants.java
@@ -4,6 +4,6 @@ public class DBConstants {
     public static final String driver = "com.mysql.cj.jdbc.Driver";
     public static final String url = "jdbc:mysql://localhost:3306/mysqlforllm";
     public static final String username = "root";
-    public static final String password = "zxcvbnm";
+    public static final String password = "rzYXj=2u-=lD";
 }
 
